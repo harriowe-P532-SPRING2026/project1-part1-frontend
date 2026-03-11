38c9b078239da6f93e9b9209cfe7be62d2541c9b
Owen Harris <owenh0410@gmail.com>
2026-03-10 22:02:00 -0400

    fixed entrypoint to fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/project1-part1-frontend/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
